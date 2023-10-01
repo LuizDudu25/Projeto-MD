58b1b4b5edbfa99d415476f4e238a1327c1fe2cd
LuizDudu25 <146662194+LuizDudu25@users.noreply.github.com>
2023-10-01 20:09:10 -0300

    Update projeto.c

--- projeto.c
+++ projeto.c
@@ -1,3 +1,64 @@
+void chavepublica ()
+{
+    long long int p, q;
+
+    printf("\nDigite dois números primos\n\n");
+    printf("p: ");
+    scanf("%lld", &p);
+
+    int a = primo (p);
+
+    while (a == 0)
+    {
+        printf("\nO número %lld não é primo! Tente novamente.\n", p);
+
+        printf("p: ");
+        scanf("%lld", &p);
+
+        a = primo (p);
+    }
+    
+    printf("q: ");
+    scanf("%lld", &q);
+
+    int b = primo(q);
+
+    while (b == 0)
+    {
+        printf("\nO número %lld não é primo! Tente novamente.\n", q);
+
+        printf("q: ");
+        scanf("%lld", &q);
+
+        b = primo(q);
+    }
+
+    long long int phi = (p - 1)*(q - 1);
+    long long int e;
+
+    printf("\nDigite um número relativamente primo a %lld:\n\ne: ", phi);
+    scanf("%lld", &e);
+
+    while (mdc(phi, e) != 1)
+    {
+        printf("\n%lld e %lld não são coprimos! Tente novamente.\n\n", phi, e);
+
+        printf("Digite um número relativamente primo a %lld:\ne: ", phi);
+        scanf("%lld", &e);
+    }
+
+    long long int n;
+    n = p*q;
+
+    FILE *chavepublica = fopen("chave.txt", "w");
+    fprintf(chavepublica, "n: %lld\ne: %lld", n, e);
+    fclose(chavepublica);
+    
+    printf("\nCHAVE PÚBLICA GERADA COM SUCESSO!\n\n");
+
+    return;
+}
+
 int main()
 {
     printf("Escolha uma das opções abaixo:\n\n");
